c19ae680ccec13a9abbe0ddf7c6710cbdba0c82d
joastek <joastek@gmail.com>
2024-02-12 21:52:10 +0100

    Add project files.

---
+++ TodoList/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Hello, World!");
+Console.WriteLine("See all TODOs");
+Console.WriteLine("Add todo");
+Console.WriteLine("Remove todo");
+Console.WriteLine("Exit");
+
+
+
+string userChoice = Console.ReadLine();
+Console.ReadKey();
