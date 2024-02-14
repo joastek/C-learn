b340fb34a69b7bcb55acc4661066ed6516e2781a
joastek <joastek@gmail.com>
2024-02-14 21:31:00 +0100

    Commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Console.WriteLine("Hello");
+Console.WriteLine("Input the first number");
+var number1= Console.ReadLine();
+int firstNumber = int.Parse(number1) ;
+if (!firstNumber)
+{
+    var sum = firstNumber + secondNumber;
+    Console.WriteLine(firstNumber + "+" + secondNumber + "=" + sum);
+}
+Console.WriteLine("Input the second number");
+var number2 = Console.ReadLine();
+int secondNumber = int.Parse(number2);
+
+Console.WriteLine("What do you want to do ?");
+Console.WriteLine("[A]dd numbers");
+Console.WriteLine("[S]ubstract numbers");
+Console.WriteLine("[M]utiply numbers");
+
+var choice= Console.ReadLine();
+if (choice =="A" || choice == "a")
+{
+    var sum = firstNumber + secondNumber;
+    Console.WriteLine(firstNumber + "+" + secondNumber + "=" +sum);
+}
+else if (choice == "S" || choice == "s")
+{
+    var sum = firstNumber / secondNumber;
+    Console.WriteLine(firstNumber + "/" + secondNumber + "=" + sum);
+}
+else if (choice == "M" || choice == "m")
+{
+    var sum = firstNumber * secondNumber;
+    Console.WriteLine(firstNumber + "*" + secondNumber + "=" + sum);
+}
+else { Console.WriteLine("Invalid choice");
+    Console.WriteLine("Press any key to close");
+}
--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,11 +1,33 @@
 ﻿
-Console.WriteLine("Hello, World!");
-Console.WriteLine("See all TODOs");
-Console.WriteLine("Add todo");
-Console.WriteLine("Remove todo");
-Console.WriteLine("Exit");
+Console.WriteLine("Provide a number.");
+string userInput = Console.ReadLine();
+int number= int.Parse(userInput);
+Console.WriteLine(number);
+//Console.WriteLine("See all TODOs");
+//Console.WriteLine("Add todo");
+//Console.WriteLine("Remove todo");
+//Console.WriteLine("Exit");
 
 
 
-string userChoice = Console.ReadLine();
-Console.ReadKey();
+//var userChoice = Console.ReadLine();
+//bool isLong=IsLong(userChoice);
+
+//bool IsLong(string input)
+//{
+//    return input.Length > 10;
+//}
+
+//var result = Add(10, 5);
+
+//Console.WriteLine("10+5=" + result);
+
+//int Add(int a, int b)
+//{ return a + b; }
+
+
+//bool isLong(string input)
+//{
+//    return input.Length > 10;
+
+//}
